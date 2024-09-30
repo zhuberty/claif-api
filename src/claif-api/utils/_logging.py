35517c997741d2b8b69412c6bf523e06834c234f
Zachary Huberty <zachary.j.huberty@gmail.com>
2024-09-29 22:32:53 -0500

    Remove cruft in _logging.py.

--- src/claif-api/utils/_logging.py
+++ src/claif-api/utils/_logging.py
@@ -10,32 +10,3 @@ logging.basicConfig(
     level=uvicorn_log_level,
     format='python:%(levelname)s:\t%(message)s',
 )
-
-# make logger accessible to other modules
-logger = logging.getLogger(__name__)
-
-# # Configure the uvicorn logger  
-# logging = logging.getLogger("uvicorn.error")
-# logging.setLevel(uvicorn_log_level)
-# logging.name = "uvicorn"
-
-# # Optionally, add a handler if not already present
-# if not logging.handlers:
-#     uvi_handler = logging.StreamHandler()
-#     uvi_handler.setLevel(uvicorn_log_level)
-#     uvi_formatter = logging.Formatter('%(name)s:%(levelname)s:\t%(message)s')
-#     uvi_handler.setFormatter(uvi_formatter)
-#     logging.addHandler(uvi_handler)
-
-# Configure the SQLAlchemy logger
-# sqlalchemy_logger = logging.getLogger("sqlalchemy.engine")
-# sqlalchemy_logger.setLevel(sql_log_level)
-# sqlalchemy_logger.name = "sqlalchemy"
-
-# Optionally, add a handler if not already present
-# if not sqlalchemy_logger.handlers:
-#     sql_handler = logging.StreamHandler()
-#     sql_handler.setLevel(sql_log_level)
-#     sql_formatter = logging.Formatter('%(name)s:%(levelname)s:\t%(message)s')
-#     sql_handler.setFormatter(sql_formatter)
-#     sqlalchemy_logger.addHandler(sql_handler)
