770f1e7486aa43723a5cd7786acca9fe1ce61d2b
Zachary Huberty <zachary.j.huberty@gmail.com>
2024-10-05 19:55:04 -0500

    Move schema.py. Update README schema.

--- src/claif-api/models/schema.py
+++ src/claif-api/models/schema.py
@@ -1,12 +1,14 @@
+from sqlalchemy import inspect
 from models.terminal_recordings import TerminalRecordingAnnotation, TerminalRecording
 from models.audio_recordings import AudioTranscription, AudioTranscriptionAnnotation, AudioFile
 from models.annotation_reviews import TerminalAnnotationReview, AudioAnnotationReview
 from models.deletion_requests import DeletionRequest
 from models.users import User
-from sqlalchemy import inspect
+
 
 def print_model_schema(model):
     """ Print the schema of a SQLAlchemy model in markdown format with sorted column names. """
+
     mapper = inspect(model)
     print(f"### {mapper.class_.__name__}")
     print("```")
